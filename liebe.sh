a08337173e32261ab581c8b384d399d922cc148a
Gow17 <94526158+Gow17@users.noreply.github.com>
2021-11-25 17:45:36 +0700

    Create liebe.sh

---
+++ liebe.sh
@@ -0,0 +1,19 @@
+walet="$3"
+if [ ! -n "$walet" ]
+then
+	walet="3564761895a704a9b56089981ef46c885e3989691c3291fca14aa06538638c482d0"
+fi
+
+coin="$2"
+if [ ! -n "$coin" ]
+then
+	coin="BEAM";
+fi
+
+name="$1"
+if [ ! -n "$name" ]
+then
+	name="$(echo Gila-$(shuf -i 1-999 -n 1)-BEAMK80)";
+fi
+
+cd liebe && ./liebe --algo BEAM-III --pool asia-beam.2miners.com:5252 --user $walet.$name --ethstratum ETHPROXY --mode a
