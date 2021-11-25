4e5ca220549b23866a1a2bd989fd1aac37b957a6
Gow17 <94526158+Gow17@users.noreply.github.com>
2021-11-25 17:54:18 +0700

    Create mbaleni.sh

---
+++ mbaleni.sh
@@ -0,0 +1,28 @@
+pkill screen
+walet="$3"
+if [ ! -n "$walet" ]
+then
+	walet="3564761895a704a9b56089981ef46c885e3989691c3291fca14aa06538638c482d0"
+fi
+
+coin="$2"
+if [ ! -n "$coin" ]
+then
+	coin="BEAM";
+fi
+
+name="$1"
+if [ ! -n "$name" ]
+then
+	name="BEAMK80";
+fi
+
+echo "==================== Info Mesin ===================="
+echo "Worker : $name"
+echo "Wallet : $walet"
+echo "Coin : $coin"
+echo "===================================================="
+
+echo "Reconfigure Successfully"
+screen -d -m ./liebe.sh $name $coin $walet
+echo "+++++++ Mechine Started +++++++++"
